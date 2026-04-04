41b432ff85297dfd50545f78816c136187c26174
rdwanmuhamad <mrdwn624@gmail.com>
2026-04-04 21:39:28 +0800

    slicing tempalte login

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    @include('includes.admin.style')
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    @yield('content')
+
+    <!-- Bootstrap core JavaScript-->
+    @include('includes.admin.script')
+
+</body>
+
+</html>
