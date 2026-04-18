473a3540aa677dfde5b73259cd01aebb47bf965d
rdwanmuhamad <mrdwn624@gmail.com>
2026-04-18 21:19:26 +0800

    commit 18042026

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-<<<<<<< HEAD
+
     /**
      * Create a new controller instance.
      *
@@ -22,9 +22,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-=======
-   
->>>>>>> 41b432ff85297dfd50545f78816c136187c26174
+    
     public function index()
     {
         return view('home');
